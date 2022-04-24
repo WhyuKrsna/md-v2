aac387684225e6e383cf07171e06efd1df84499b
WhyuKrsna <103726209+WhyuKrsna@users.noreply.github.com>
2022-04-24 19:28:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6283182270674'] // Put your number here
+global.owner = ['6287784550689'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6287784550689'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -42,12 +42,12 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'stickerby'
-global.author = `𝙿𝚊𝚝𝚛𝚒𝚌𝚔 𝙱𝚘𝚝`
-global.wm = '𝙿𝚊𝚝𝚛𝚒𝚌𝚔 𝙱𝚘𝚝'
+global.packname = 'KrisnaBot'
+global.author = `𝕂𝕣𝕚𝕤𝕟𝕒 𝔹𝕠𝕥`
+global.wm = '𝕂𝕣𝕚𝕤𝕟𝕒 𝔹𝕠𝕥'
 global.watermark = wm
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/hyzer-stfu'
+global.media = 'https://i.ibb.co/JcnkVwd/elyas.jpg'
+global.instagram = 'https://instagram.com/wahyu.jr86'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
